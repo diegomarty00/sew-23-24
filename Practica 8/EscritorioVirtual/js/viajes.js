2524be00ed797a202215cb714f1da37dff14a971
Diego Martínez <uo270457@uniovi.es>
2023-12-15 18:51:19 +0100

    Me quedan por añadir las fotos de viajes.js

--- Practica 8/EscritorioVirtual/js/viajes.js
+++ Practica 8/EscritorioVirtual/js/viajes.js
@@ -72,20 +72,20 @@ class Viajes {
         }
     }
 
-    verTodo(dondeVerlo){
-        var ubicacion=document.getElementById(dondeVerlo);
-        var datos='<p>'+ this.mensaje + '</p>'; 
-        datos+='<p>Longitud: '+this.longitud +' grados</p>'; 
-        datos+='<p>Latitud: '+this.latitud +' grados</p>';
-        datos+='<p>Precisión de la longitud y latitud: '+ this.precision +' metros</p>';
-        datos+='<p>Altitud: '+ this.altitude +' metros</p>';
-        datos+='<p>Precisión de la altitud: '+ this.precisionAltitud +' metros</p>'; 
-        datos+='<p>Rumbo: '+ this.rumbo +' grados</p>'; 
-        datos+='<p>Velocidad: '+ this.velocidad +' metros/segundo</p>';
+    verTodo(dondeVerlo) {
+        var ubicacion = document.getElementById(dondeVerlo);
+        var datos = '<p>' + this.mensaje + '</p>';
+        datos += '<p>Longitud: ' + this.longitud + ' grados</p>';
+        datos += '<p>Latitud: ' + this.latitud + ' grados</p>';
+        datos += '<p>Precisión de la longitud y latitud: ' + this.precision + ' metros</p>';
+        datos += '<p>Altitud: ' + this.altitude + ' metros</p>';
+        datos += '<p>Precisión de la altitud: ' + this.precisionAltitud + ' metros</p>';
+        datos += '<p>Rumbo: ' + this.rumbo + ' grados</p>';
+        datos += '<p>Velocidad: ' + this.velocidad + ' metros/segundo</p>';
         ubicacion.innerHTML = datos;
     }
-    getMapaEstaticoGoogle(dondeVerlo){
-        var ubicacion=document.getElementById(dondeVerlo);
+    getMapaEstaticoGoogle(dondeVerlo) {
+        var ubicacion = document.getElementById(dondeVerlo);
         //URL: obligatoriamente https
         var url = "https://maps.googleapis.com/maps/api/staticmap?";
         //Parámetros
@@ -93,9 +93,9 @@ class Viajes {
         var centro = "center=" + this.latitud + "," + this.longitud;
         //zoom (obligatorio si no hay marcadores)
         //zoom: 1 (el mundo), 5 (continentes), 10 (ciudad), 15 (calles), 20 (edificios)
-        var zoom ="&zoom=15";
+        var zoom = "&zoom=15";
         //Tamaño del mapa en pixeles (obligatorio)
-        var tamaño= "&size=800x600";
+        var tamaño = "&size=800x600";
         //Escala (opcional)
         //Formato (opcional): PNG,JPEG,GIF
         //Tipo de mapa (opcional)
@@ -106,14 +106,14 @@ class Viajes {
         //rutas. path (opcional)
         //visible (optional)
         //style (opcional)
-        var sensor = "&sensor=false"; 
-        
+        var sensor = "&sensor=false";
+
         this.imagenMapa = url + centro + zoom + tamaño + marcador + sensor + "&key=" + this.token;
-        ubicacion.innerHTML = "<img src='"+this.imagenMapa+"' alt='mapa estático google' />";
+        ubicacion.innerHTML = "<img src='" + this.imagenMapa + "' alt='mapa estático google' />";
     }
 
-    getMapaDinamicoGoogle(){    
-        let posicion = { lat: this.latitud, lng: this.longitud}
+    getMapaDinamicoGoogle() {
+        let posicion = { lat: this.latitud, lng: this.longitud }
         this.mapaGeoposicionado = new google.maps.Map(document.getElementById('dinamico'), {
             zoom: 13,
             center: posicion,
@@ -127,7 +127,7 @@ class Viajes {
     }
 
     leerArchivoKML(files) {
-        for (let i = 0; i < files.length; i++){
+        for (let i = 0; i < files.length; i++) {
             let file = files[i];
             let fr = new FileReader();
             fr.onload = this.onFileLoad.bind(this, this.mapaGeoposicionado);
@@ -135,17 +135,17 @@ class Viajes {
         }
     }
 
-    onFileLoad(mapa, evento){
+    onFileLoad(mapa, evento) {
         const colores = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF'];
         let kml = $(evento.target.result);
         let coordinates = kml.find('coordinates').text().trim().split('\n');
         let coordinatesArray = []
         let nuevoCentro;
-        for (let i = 0; i < coordinates.length; i++){
+        for (let i = 0; i < coordinates.length; i++) {
             let coord = coordinates[i];
             let [lng, lat, alt] = coord.split(",").map(parseFloat);
-            coordinatesArray.push({lat, lng, alt});
-            nuevoCentro = {lat: lat, lng: lng};
+            coordinatesArray.push({ lat, lng, alt });
+            nuevoCentro = { lat: lat, lng: lng };
         }
         let color = colores[this.colorIndex % colores.length];
         this.colorIndex += 1;
@@ -163,7 +163,7 @@ class Viajes {
         let heading = $("<h4>").text("Perfiles SVG");
         let section = $("<section>").attr("data-element", "archivosSVG");
         section.append(heading);
-        for (let i = 0; i < files.length; i++){
+        for (let i = 0; i < files.length; i++) {
             let file = files[i];
             let lector = new FileReader();
             lector.onload = function (evento) {
@@ -179,24 +179,23 @@ class Viajes {
     }
 
     leerArchivoXML(files) {
-        if (window.File && window.FileReader && window.FileList && window.Blob) 
-        {  
+        if (window.File && window.FileReader && window.FileList && window.Blob) {
             //El navegador soporta el API File
-            let archivo = files[0];    
+            let archivo = files[0];
             if (archivo) {
                 let lector = new FileReader();
                 lector.readAsText(archivo);
-                lector.onload = function(evento) {
+                lector.onload = function (evento) {
                     let xml = $(lector.result);
                     let seccion = $("<section>").attr("data-element", "rutas");
                     let titleRutas = $("<h3>").text("Rutas");
                     seccion.append(titleRutas);
-                    $('ruta', xml).each( (index, ruta) => {
+                    $('ruta', xml).each((index, ruta) => {
                         const rutaXML = $(ruta);
-            
+
                         let article = $("<article>").attr("data-element", "infoRuta");
-        
-                        let nombre = $("<h4>").text(`Ruta ${index+1}: ${rutaXML.attr("nombreRuta")}`).attr("data-element", "nombre-ruta");
+
+                        let nombre = $("<h4>").text(`Ruta ${index + 1}: ${rutaXML.attr("nombreRuta")}`).attr("data-element", "nombre-ruta");
                         article.append(nombre);
 
                         let tipo = $("<p>").text(`Tipo de ruta: ${rutaXML.find("tipoRuta").text()}`).attr("data-element", "tipo-ruta");
@@ -226,13 +225,13 @@ class Viajes {
                         let agencia = $("<p>").text(`Agencia: ${rutaXML.find('agencia').text()}`).attr("data-element", "agencia-ruta");
                         article.append(agencia);
 
-                        let coordenadas = $("<p>").text(`Coordenadas: ${rutaXML.find('coordenadasRuta > latitud').text()}, ${rutaXML.find('coordenadasRuta > longitud').text()}`).attr("data-element", "coordenadas-ruta"); 
+                        let coordenadas = $("<p>").text(`Coordenadas: ${rutaXML.find('coordenadasRuta > latitud').text()}, ${rutaXML.find('coordenadasRuta > longitud').text()}`).attr("data-element", "coordenadas-ruta");
                         article.append(coordenadas);
 
-                        let altitud = $("<p>").text(`Altitud: ${rutaXML.find('coordenadasRuta > altitud').text()}`).attr("data-element", "altitud-ruta"); 
-                        article.append(coordenadas);
+                        let altitud = $("<p>").text(`Altitud: ${rutaXML.find('coordenadasRuta > altitud').text()} metros`).attr("data-element", "altitud-ruta");
+                        article.append(altitud);
 
-                        let sugerencias = $("<p>").text(`Sugerencias: ${rutaXML.find('sugerencias').text()}`).attr("data-element", "sugerencias-ruta"); 
+                        let sugerencias = $("<p>").text(`Sugerencias: ${rutaXML.find('sugerencias').text()}`).attr("data-element", "sugerencias-ruta");
                         article.append(sugerencias);
 
 
@@ -241,45 +240,43 @@ class Viajes {
                         article.append(recomendacion);
 
                         let referecias = $("<ul>").attr("data-element", "lista-referencias");
-                        
+
                         rutaXML.find("referencia").each((index, ref) => {
                             let lista = $("<li>").attr("data-element", "referencias-ruta");
-                            
-                            let enlace = $("<a>").text(`Referencia ${index+1}`).attr("href", ref.outerText);
+
+                            let enlace = $("<a>").text(`Referencia ${index + 1}`).attr("href", ref.outerText);
                             lista.append(enlace);
                             referecias.append(lista);
                         });
                         article.append(referecias);
 
                         let galeria = $("<section>").attr("data-element", "galeria");
-                        let title = $("<h5>").text("Galería de fotos");
-                        galeria.append(title);
+                        let fotos = $("<h5>").text("Galería de fotos");
+                        galeria.append(fotos);
 
                         let seccionHitos = $("<section>").attr("data-element", "hitos");
-                        let titleHitos = $("<h5>").text("Hitos");
-                        seccionHitos.append(titleHitos);
-                        let hitos = $("<ul>").attr("data-element", "listaHitos");
+                        let hitos = $("<h5>").text("Hitos");
                         seccionHitos.append(hitos);
-                        let listaHitos = rutaXML.find('hitos');
-                        listaHitos.find('hito').each((index, hito) => {
+                        rutaXML.find('hito').each((index, hito) => {
+                            let articuloHitos = $("<article>").attr("data-element", "articuloHitos");
+                            seccionHitos.append(articuloHitos);
                             const hitoXML = $(hito);
-                            let nombreHito = $("<li>").text(hitoXML.find('nombre').text());
-        
-                            let caracteristicasHito = $("<ul>");
-                            let descripcion = $("<li>").text(hitoXML.find('descripcion').text());
-                            caracteristicasHito.append(descripcion);
-        
-                            let coordenadas = $("<li>").text(`Coordenadas: ${hitoXML.find('coordenadas').attr('latitud')}, ${hitoXML.find('coordenadas').attr('longitud')}`);
-                            caracteristicasHito.append(coordenadas);
-        
-                            let altitud = $("<li>").text(`Altitud: ${hitoXML.find('coordenadas').attr('altitud')}m`);
-                            caracteristicasHito.append(altitud);
-        
-                            let distanciaHitoAnterior = $("<li>").text(`Distancia al hito anterior: ${hitoXML.find('distancia-hito-anterior').text()}${hitoXML.find('distancia-hito-anterior').attr('unidad')}`);
-                            caracteristicasHito.append(distanciaHitoAnterior);
-        
-                            nombreHito.append(caracteristicasHito);
-                            hitos.append(nombreHito);
+                            //${hitoXML.attr("nombreRuta")}`).attr("data-element", "nombre-ruta");
+                            let nombreHito = $("<h6>").text(`${hitoXML.attr("nombreHito")}`);
+                            articuloHitos.append(nombreHito);
+                            let descripcionHito = $("<p>").text(hitoXML.find('descripcionHito').text());
+                            articuloHitos.append(descripcionHito);
+
+                            let coordenadasHito = $("<p>").text(`Coordenadas: ${hitoXML.find('coordenadasHito > latitud').text()}, ${hitoXML.find('coordenadasHito > longitud').text()}`);
+                            articuloHitos.append(coordenadasHito);
+
+                            let altitud = $("<p>").text(`Altitud: ${hitoXML.find('coordenadasHito > altitud').text()} metros`);
+                            articuloHitos.append(altitud);
+
+                            let distanciaHito = $("<p>").text(`Distancia de la salida: ${hitoXML.find('distancia').text()}`);
+                            articuloHitos.append(distanciaHito);
+
+                            
 
                             let listaFotos = hitoXML.find('galeria-fotos');
                             listaFotos.find('foto').each((index, foto) => {
